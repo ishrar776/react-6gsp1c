32847a1abcd50490d82629b1bb08d81aead3fd5b
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-14 07:57:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [values, setValues] = useState({
+    message: '',
+  });
+  const [isSubmit, setIsSubmit] = useState(false);
+  const handleChanges = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  const submitdata = (e) => {
+    e.preventDefault();
+    setIsSubmit(true);
+    if (isSubmit) {
+      console.log('israr hey');
+      const allData = { ...values };
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(allData),
+      };
+      console.log(requestOptions.body);
+      fetch('https://reqres.in/api/posts', requestOptions)
+        .then((response) => {
+          console.log('post sucess' + response);
+          response.json();
+        })
+        //.then((data) => setValues(data.message))
+        .catch((error) => {
+          console.log('failed result' + error);
+        });
+    }
+  };
+  return (
+    <form onSubmit={submitdata}>
+      <h1>Hello StackBlitz!</h1>
+      <p>
+        <input
+          type="text"
+          onChange={handleChanges}
+          value={values.message}
+          name="message"
+        ></input>
+      </p>
+      <p>
+        <input type="submit" />
+      </p>
+    </form>
+  );
+}
